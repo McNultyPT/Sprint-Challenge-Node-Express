d129a116a5928956d5aaab2bb09f0295acf96fd6
McNultyPT <mcnultypt@gmail.com>
2019-02-22 08:38:13 -0800

    adding method to get action by id

--- data/helpers/action-router.js
+++ data/helpers/action-router.js
@@ -4,4 +4,29 @@ const Actions = require('./actionModel.js');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const actions = await Actions.get(req.query);
+        res.status(200).json(actions);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error retrieving the actions.' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const action = await Actions.get(req.params.id);
+
+        if (action) {
+            res.status(200).json(action);
+        } else {
+            res.status(404).json({ message: 'Action not found.' });
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error retrieving the action.' });
+    }
+});
+
 module.exports = router;
