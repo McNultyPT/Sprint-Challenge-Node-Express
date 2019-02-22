9e6a5f9c67ad431c0a780054c64b5f2a4a273669
McNultyPT <mcnultypt@gmail.com>
2019-02-22 08:27:25 -0800

    setting up router for actions and projects

---
+++ data/helpers/action-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const Actions = require('./actionModel.js');
+
+const router = express.Router();
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const actionsRouter = require('./data/helpers/action-router.js');
+const projectsRouter = require('./data/helpers/project-router.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
     res.send(`
